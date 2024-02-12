06ed268b294e9ce5cae2f614d031ebf082d8cba3
IvanKornev <brovkin.komar@yandex.ru>
2024-02-12 11:36:31 +0300

    feat: валидирую форму обр.связи

--- back/app/Http/Controllers/FeedbackController.php
+++ back/app/Http/Controllers/FeedbackController.php
@@ -5,9 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller;
 
+use App\Http\Requests\FeedbackRequest;
+
 class FeedbackController extends Controller
 {
-    public function store(): JsonResponse
+    /**
+     * Добавляет обращение через форму обр.связи
+     *
+     * @return JsonResponse
+     */
+    public function store(FeedbackRequest $request): JsonResponse
     {
         return response()->json(['message' => 'ok']);
     }
